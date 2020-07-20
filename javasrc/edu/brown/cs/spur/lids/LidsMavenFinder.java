7cb0ee1c51f88e710c8a369991341707501fcc87
Steven Reiss <spr@cs.brown.edu>
2020-07-20 13:05:07 -0400

    Change max to 4000 from 5000.

--- javasrc/edu/brown/cs/spur/lids/LidsMavenFinder.java
+++ javasrc/edu/brown/cs/spur/lids/LidsMavenFinder.java
@@ -1,34 +1,34 @@
 /********************************************************************************/
-/*                                                                              */
-/*              LidsMavenFinder.java                                            */
-/*                                                                              */
-/*      Maven search interface for Lids                                         */
-/*                                                                              */
+/*										*/
+/*		LidsMavenFinder.java						*/
+/*										*/
+/*	Maven search interface for Lids 					*/
+/*										*/
 /********************************************************************************/
-/*      Copyright 2013 Brown University -- Steven P. Reiss                    */
+/*	Copyright 2013 Brown University -- Steven P. Reiss		      */
 /*********************************************************************************
- *  Copyright 2013, Brown University, Providence, RI.                            *
- *                                                                               *
- *                        All Rights Reserved                                    *
- *                                                                               *
- *  Permission to use, copy, modify, and distribute this software and its        *
- *  documentation for any purpose other than its incorporation into a            *
- *  commercial product is hereby granted without fee, provided that the          *
- *  above copyright notice appear in all copies and that both that               *
- *  copyright notice and this permission notice appear in supporting             *
- *  documentation, and that the name of Brown University not be used in          *
- *  advertising or publicity pertaining to distribution of the software          *
- *  without specific, written prior permission.                                  *
- *                                                                               *
- *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS                *
- *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND            *
- *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY      *
- *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY          *
- *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,              *
- *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS               *
- *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE          *
- *  OF THIS SOFTWARE.                                                            *
- *                                                                               *
+ *  Copyright 2013, Brown University, Providence, RI.				 *
+ *										 *
+ *			  All Rights Reserved					 *
+ *										 *
+ *  Permission to use, copy, modify, and distribute this software and its	 *
+ *  documentation for any purpose other than its incorporation into a		 *
+ *  commercial product is hereby granted without fee, provided that the 	 *
+ *  above copyright notice appear in all copies and that both that		 *
+ *  copyright notice and this permission notice appear in supporting		 *
+ *  documentation, and that the name of Brown University not be used in 	 *
+ *  advertising or publicity pertaining to distribution of the software 	 *
+ *  without specific, written prior permission. 				 *
+ *										 *
+ *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS		 *
+ *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND		 *
+ *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY	 *
+ *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY 	 *
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,		 *
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS		 *
+ *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 	 *
+ *  OF THIS SOFTWARE.								 *
+ *										 *
  ********************************************************************************/
 
 
@@ -59,9 +59,9 @@ class LidsMavenFinder implements LidsConstants
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Private Storage                                                         */
-/*                                                                              */
+/*										*/
+/*	Private Storage 							*/
+/*										*/
 /********************************************************************************/
 
 private static Map<String,List<LidsLibrary>> found_libs = new HashMap<>();
@@ -69,19 +69,19 @@ private static Map<String,MavenLibrary> known_libs = new HashMap<>();
 private static KeySearchCache cose_cache = KeySearchCache.getCache();
 private static Map<String,List<LidsLibrary>> lib_byname = new HashMap<>();
 
-private static final int MAX_RESULTS = 5000;
+private static final int MAX_RESULTS = 4000;
 
 private static String SEARCH_PFX =
-   "https://search.maven.org/solrsearch/select?rows=100&wt=json&q=";  
+   "https://search.maven.org/solrsearch/select?rows=100&wt=json&q=";
 
 
 
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Constructors                                                            */
-/*                                                                              */
+/*										*/
+/*	Constructors								*/
+/*										*/
 /********************************************************************************/
 
 LidsMavenFinder()
@@ -91,9 +91,9 @@ LidsMavenFinder()
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Find library for a path                                                 */
-/*                                                                              */
+/*										*/
+/*	Find library for a path 						*/
+/*										*/
 /********************************************************************************/
 
 List<LidsLibrary> findLibrariesForImport(String imp)
@@ -102,9 +102,9 @@ List<LidsLibrary> findLibrariesForImport(String imp)
       if (!imp.contains(".")) break;
       List<LidsLibrary> libs = null;
       if (i == 0) {
-         if (!imp.endsWith(".*")) libs = checkLibrary(imp,true);
+	 if (!imp.endsWith(".*")) libs = checkLibrary(imp,true);
        }
-      else libs = checkLibrary(imp,false);   
+      else libs = checkLibrary(imp,false);
       if (libs != null) return libs;
       int idx = imp.lastIndexOf(".");
       if (idx < 0) break;
@@ -119,13 +119,13 @@ List<LidsLibrary> findLibrariesForImport(String imp)
 private synchronized List<LidsLibrary> checkLibrary(String path,boolean iscls)
 {
    if (found_libs.containsKey(path)) return found_libs.get(path);
-   
+
    List<LidsLibrary> rslt = doMavenSearch(path);
-   
+
    if (rslt != null && rslt.size() == 0) rslt = null;
-   
+
    found_libs.put(path,rslt);
-   
+
    return rslt;
 }
 
@@ -138,65 +138,65 @@ private List<LidsLibrary> doMavenSearch(String path)
 
    Map<String,Set<String>> artifacts = new HashMap<>();
    Set<String> related = new HashSet<>();
-   int nrslt =  1;
+   int nrslt =	1;
    int start = 0;
    while (start < nrslt) {
       String q1 = q;
-      if (start > 0) 
-         q1 = q1 + "&start=" + start;
+      if (start > 0)
+	 q1 = q1 + "&start=" + start;
       String rslt1 = getMavenResult(q1);
       if (rslt1 == null) break;
       try {
-         JSONObject top = new JSONObject(rslt1);
-         JSONObject resp = top.getJSONObject("response");
-         JSONArray arr = resp.getJSONArray("docs");
-         if (start == 0) {
-            nrslt = resp.getInt("numFound");
-            if (nrslt > MAX_RESULTS) nrslt = MAX_RESULTS;
-          }        
-         start += arr.length();
-         if (arr != null) {
-            for (int i = 0; i < arr.length(); ++i) {
-               JSONObject doc = arr.getJSONObject(i);
-               String type = doc.getString("p");
-               if (type == null || !type.equals("jar")) continue;
-               String art = doc.getString("a");
-               String grp = doc.getString("g");
-               // String ver = doc.getString("v");
-               if (CoseConstants.isRelatedPackage(grp,path)) related.add(grp);
-               Set<String> al = artifacts.get(grp);
-               if (al == null) {
-                  al = new HashSet<>();
-                  artifacts.put(grp,al);
-                }
-               al.add(art);
-             }
-          }
+	 JSONObject top = new JSONObject(rslt1);
+	 JSONObject resp = top.getJSONObject("response");
+	 JSONArray arr = resp.getJSONArray("docs");
+	 if (start == 0) {
+	    nrslt = resp.getInt("numFound");
+	    if (nrslt > MAX_RESULTS) nrslt = MAX_RESULTS;
+	  }	
+	 start += arr.length();
+	 if (arr != null) {
+	    for (int i = 0; i < arr.length(); ++i) {
+	       JSONObject doc = arr.getJSONObject(i);
+	       String type = doc.getString("p");
+	       if (type == null || !type.equals("jar")) continue;
+	       String art = doc.getString("a");
+	       String grp = doc.getString("g");
+	       // String ver = doc.getString("v");
+	       if (CoseConstants.isRelatedPackage(grp,path)) related.add(grp);
+	       Set<String> al = artifacts.get(grp);
+	       if (al == null) {
+		  al = new HashSet<>();
+		  artifacts.put(grp,al);
+		}
+	       al.add(art);
+	     }
+	  }
        }
       catch (JSONException e) {
-         IvyLog.logE("Problem with maven result: " + e);
+	 IvyLog.logE("Problem with maven result: " + e);
        }
     }
    List<LidsLibrary> rslt = new ArrayList<>();
-   
+
    if (!related.isEmpty()) {
       for (Iterator<String> it = artifacts.keySet().iterator(); it.hasNext(); ) {
-         String art = it.next();
-         if (!related.contains(art)) it.remove();
+	 String art = it.next();
+	 if (!related.contains(art)) it.remove();
        }
     }
-   
+
    for (Map.Entry<String,Set<String>> ent : artifacts.entrySet()) {
       List<LidsLibrary> alllibs = getLibraryFromName(ent.getKey());
       for (LidsLibrary ll : alllibs) {
-         if (ent.getValue().contains(ll.getName())) {
-            rslt.add(ll);
-          }
+	 if (ent.getValue().contains(ll.getName())) {
+	    rslt.add(ll);
+	  }
        }
     }
-   
+
    if (rslt.isEmpty()) rslt = null;
-   
+
    return rslt;
 }
 
@@ -206,7 +206,7 @@ private List<LidsLibrary> getLibraryFromName(String name)
 {
    List<LidsLibrary> rslt = lib_byname.get(name);
    if (rslt != null) return rslt;
-   
+
    rslt = new ArrayList<>();
    String q1 = SEARCH_PFX + "g:%22" + name + "%22";
    String rslt2 = getMavenResult(q1);
@@ -215,23 +215,23 @@ private List<LidsLibrary> getLibraryFromName(String name)
       JSONObject resp = top.getJSONObject("response");
       JSONArray arr = resp.optJSONArray("docs");
       if (arr != null) {
-         for (int i = 0; i < arr.length(); ++i) {
-            JSONObject doc = arr.getJSONObject(i);
-            String type = doc.getString("p");
-            if (type == null || !type.equals("jar")) continue;
-            String id = doc.getString("id");
-            MavenLibrary fnd = known_libs.get(id);
-            if (fnd == null) {
-               fnd = new MavenLibrary(doc);
-               known_libs.put(id,fnd);
-             }
-            rslt.add(fnd);
-          }
+	 for (int i = 0; i < arr.length(); ++i) {
+	    JSONObject doc = arr.getJSONObject(i);
+	    String type = doc.getString("p");
+	    if (type == null || !type.equals("jar")) continue;
+	    String id = doc.getString("id");
+	    MavenLibrary fnd = known_libs.get(id);
+	    if (fnd == null) {
+	       fnd = new MavenLibrary(doc);
+	       known_libs.put(id,fnd);
+	     }
+	    rslt.add(fnd);
+	  }
        }
     }
-   
+
    lib_byname.put(name,rslt);
-   
+
    return rslt;
 }
 
@@ -241,48 +241,48 @@ private String getMavenResult(String q)
 {
    ByteArrayOutputStream baos = new ByteArrayOutputStream();
    URL url = null;
-   
+
    long delay = 30000;
    for ( ; ; ) {
       try {
-         url = new URL(q);
-         InputStream br = cose_cache.getInputStream(url,true,false);
-         byte [] buf = new byte[8192];
-         for ( ; ; ) {
-            int ln = br.read(buf);
-            if (ln <= 0) break;
-            baos.write(buf,0,ln);
-          }
-         br.close();
-         String rslt = baos.toString("UTF-8");
-         if (rslt.length() == 0) return null;
-         if (!rslt.endsWith("\n")) rslt += "\n";
-         return rslt;
+	 url = new URL(q);
+	 InputStream br = cose_cache.getInputStream(url,true,false);
+	 byte [] buf = new byte[8192];
+	 for ( ; ; ) {
+	    int ln = br.read(buf);
+	    if (ln <= 0) break;
+	    baos.write(buf,0,ln);
+	  }
+	 br.close();
+	 String rslt = baos.toString("UTF-8");
+	 if (rslt.length() == 0) return null;
+	 if (!rslt.endsWith("\n")) rslt += "\n";
+	 return rslt;
        }
       catch (Exception e) {
-         if (delay < 30000 && e.toString().contains(" 504 ")) {
-            try {
-               Thread.sleep(delay);
-             }
-            catch (InterruptedException ex) { }
-            delay = 2*delay;
-            continue;
-          }
-         cose_cache.setCacheContents(url,"\n");
-         IvyLog.logI("LIDS","Problem getting MAVEN data: " + e);
-         break;
+	 if (delay < 30000 && e.toString().contains(" 504 ")) {
+	    try {
+	       Thread.sleep(delay);
+	     }
+	    catch (InterruptedException ex) { }
+	    delay = 2*delay;
+	    continue;
+	  }
+	 cose_cache.setCacheContents(url,"\n");
+	 IvyLog.logI("LIDS","Problem getting MAVEN data: " + e);
+	 break;
        }
     }
-   
+
    return null;
 }
 
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Library implementation for Maven results                                */
-/*                                                                              */
+/*										*/
+/*	Library implementation for Maven results				*/
+/*										*/
 /********************************************************************************/
 
 private static class MavenLibrary extends  LidsLibrary {
@@ -291,7 +291,7 @@ private static class MavenLibrary extends  LidsLibrary {
    private String lib_name;
    private long lib_timestamp;
    private String lib_version;
-   
+
    MavenLibrary(JSONObject obj) {
       lib_id = obj.getString("id");
       lib_name = obj.getString("a");
@@ -299,21 +299,21 @@ private static class MavenLibrary extends  LidsLibrary {
       lib_version = obj.optString("v",null);
       lib_version = obj.optString("latestVersion",lib_version);
     }
-   
-   @Override public String getName()            { return lib_name; }
-   @Override public String getVersion()         { return lib_version; }
-   @Override public String getId()              { return lib_id; }
-   @Override public String getFullId()          { return lib_id + ":" + lib_version; }
+
+   @Override public String getName()		{ return lib_name; }
+   @Override public String getVersion() 	{ return lib_version; }
+   @Override public String getId()		{ return lib_id; }
+   @Override public String getFullId()		{ return lib_id + ":" + lib_version; }
 
    @Override public String toString() {
       return lib_id + "@" + lib_name + "@" + lib_timestamp + "@" + lib_version;
     }
-   
-}       // end of inner class MavenLibrary
+
+}	// end of inner class MavenLibrary
 
 
 
-}       // end of class LidsMavenFinder
+}	// end of class LidsMavenFinder
 
 
 
