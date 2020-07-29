2603a0be614c6db3c4dee70c72f38c3d93e3d403
Steven Reiss <spr@cs.brown.edu>
2020-07-29 14:22:54 -0400

    Update compile fail/succeed log

--- javasrc/edu/brown/cs/spur/stare/StareSolutionBase.java
+++ javasrc/edu/brown/cs/spur/stare/StareSolutionBase.java
@@ -1,21 +1,21 @@
 /********************************************************************************/
-/*                                                                              */
-/*              StareSolutionBase.java                                          */
-/*                                                                              */
-/*      Hold information about a potential solution                             */
-/*                                                                              */
+/*										*/
+/*		StareSolutionBase.java						*/
+/*										*/
+/*	Hold information about a potential solution				*/
+/*										*/
 /********************************************************************************/
-/*      Copyright 2011 Brown University -- Steven P. Reiss                    */
+/*	Copyright 2011 Brown University -- Steven P. Reiss		      */
 /*********************************************************************************
- *  Copyright 2011, Brown University, Providence, RI.                            *
- *                                                                               *
- *                        All Rights Reserved                                    *
- *                                                                               *
- * This program and the accompanying materials are made available under the      *
+ *  Copyright 2011, Brown University, Providence, RI.				 *
+ *										 *
+ *			  All Rights Reserved					 *
+ *										 *
+ * This program and the accompanying materials are made available under the	 *
  * terms of the Eclipse Public License v1.0 which accompanies this distribution, *
- * and is available at                                                           *
- *      http://www.eclipse.org/legal/epl-v10.html                                *
- *                                                                               *
+ * and is available at								 *
+ *	http://www.eclipse.org/legal/epl-v10.html				 *
+ *										 *
  ********************************************************************************/
 
 /* SVN: $Id$ */
@@ -65,28 +65,28 @@ class StareSolutionBase implements StareConstants, StareSolution
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Private Storage                                                         */
-/*                                                                              */
+/*										*/
+/*	Private Storage 							*/
+/*										*/
 /********************************************************************************/
 
-private SumpModel       for_model;
-private SumpModel       pattern_model;
-private CoseResult      cose_result;
-private CoseResult      local_tests;
-private CoseResult      global_tests;
+private SumpModel	for_model;
+private SumpModel	pattern_model;
+private CoseResult	cose_result;
+private CoseResult	local_tests;
+private CoseResult	global_tests;
 private Map<String,String> name_map;
-private File            work_directory;
+private File		work_directory;
 private VelocityContext map_context;
 
-private Random          random_gen;
+private Random		random_gen;
 
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Constructors                                                            */
-/*                                                                              */
+/*										*/
+/*	Constructors								*/
+/*										*/
 /********************************************************************************/
 
 StareSolutionBase(StareCandidateSolution sc)
@@ -101,37 +101,37 @@ StareSolutionBase(StareCandidateSolution sc)
    work_directory = null;
    map_context = null;
 }
- 
-
-
-/********************************************************************************/
-/*                                                                              */
-/*      Access methods                                                          */
-/*                                                                              */
-/********************************************************************************/
-
-@Override public SumpModel getModel()                   { return for_model; }
-@Override public CoseResult getCoseResult()             { return cose_result; }
-@Override public CoseResult getLocalTestResult()        { return local_tests; }
-@Override public CoseResult getGlobalTestResult()       { return global_tests; }
-@Override public Map<String,String> getNameMap()        { return name_map; }
-@Override public SumpModel getPatternModel()            { return pattern_model; }
-
 
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Methods to generate code                                                */
-/*                                                                              */
+/*										*/
+/*	Access methods								*/
+/*										*/
+/********************************************************************************/
+
+@Override public SumpModel getModel()			{ return for_model; }
+@Override public CoseResult getCoseResult()		{ return cose_result; }
+@Override public CoseResult getLocalTestResult()	{ return local_tests; }
+@Override public CoseResult getGlobalTestResult()	{ return global_tests; }
+@Override public Map<String,String> getNameMap()	{ return name_map; }
+@Override public SumpModel getPatternModel()		{ return pattern_model; }
+
+
+
+
+/********************************************************************************/
+/*										*/
+/*	Methods to generate code						*/
+/*										*/
 /********************************************************************************/
 
 @Override public boolean generateCode()
 {
    boolean rsts = false;
-   
+
    map_context = new VelocityContext();
-   
+
    try {
       clearOldCode();
       CompilationUnit cu = (CompilationUnit) cose_result.getStructure();
@@ -141,23 +141,24 @@ StareSolutionBase(StareCandidateSolution sc)
       producePomFile();
       // generate resource files
       // generate test files
-      
+
       File dir = (File) map_context.get("DIRECTORY");
       IvyExec ex = new IvyExec("mvn compile",dir,IvyExec.ERROR_OUTPUT);
       int sts = ex.waitFor();
       if (sts != 0) {
-         IvyLog.logI("Mavan failed");
-         rsts = false;
+	 IvyLog.logI("Maven compile failed");
+	 rsts = false;
        }
       else {
-         rsts = true;
+	 IvyLog.logI("Maven compile succeeded");
+	 rsts = true;
        }
       // run tests
     }
    catch (IOException e) {
       return false;
     }
-   
+
    return rsts;
 }
 
@@ -176,7 +177,7 @@ private void setup(CompilationUnit cu)
 {
    File dir = getDirectory();
    map_context.put("DIRECTORY",dir);
-   
+
    File topdir = new File(dir,"src");
    File srcdir =  new File(topdir,"main");
    File resdir = new File(srcdir,"resources");
@@ -187,7 +188,7 @@ private void setup(CompilationUnit cu)
    localtestdir = new File(localtestdir,"java");
    File globaltestdir = new File(topdir,"globaltest");
    globaltestdir = new File(globaltestdir,"java");
-   
+
    PackageDeclaration pd = cu.getPackage();
    if (pd != null) {
       String pkg = pd.getName().getFullyQualifiedName();
@@ -196,15 +197,15 @@ private void setup(CompilationUnit cu)
       map_context.put("PACKAGEDOT",pkg + ".");
       StringTokenizer tok = new StringTokenizer(pkg,".");
       while (tok.hasMoreTokens()) {
-         String s = tok.nextToken();
-         srcdir = new File(srcdir,s);
-         testdir = new File(testdir,s);
+	 String s = tok.nextToken();
+	 srcdir = new File(srcdir,s);
+	 testdir = new File(testdir,s);
        }
     }
    else {
       map_context.remove("PACKAGE");
     }
-   
+
    map_context.put("TOPDIR",topdir);
    srcdir.mkdirs();
    map_context.put("SRCDIR",srcdir);
@@ -219,7 +220,7 @@ private void setup(CompilationUnit cu)
    File f = new File(dir,"bin");
    f.mkdirs();
    map_context.put("BINDIR",f);
-   
+
    try {
       File link = new File(dir,"tosrc");
       Path p1 = link.toPath();
@@ -231,14 +232,14 @@ private void setup(CompilationUnit cu)
       Files.createSymbolicLink(p3,p4);
     }
    catch (IOException e) { }
-   
+
    File pomfile = new File(dir,"pom.xml");
    map_context.put("POMFILE",pomfile);
-   
+
    String v = System.getProperty("java.specification.version");
    map_context.put("JAVAVERSION",v);
    map_context.put("ENCODING","UTF-8");
-   
+
    SumpData smd = for_model.getModelData();
    List<LidsLibrary> libs = new ArrayList<>();
    boolean havejunit = false;
@@ -250,25 +251,25 @@ private void setup(CompilationUnit cu)
       LidsLibrary ju = LidsFinder.createLibrary("junit:junit:4.13");
       libs.add(ju);
     }
-    
+
    map_context.put("LIBRARIES",libs);
    map_context.put("SCORE",smd.getModelScore());
-   
+
    map_context.put("MAXTIME","10000L");
-   
+
    StringBuffer buf = new StringBuffer();
    for (Object o : cu.imports()) {
       ImportDeclaration id = (ImportDeclaration) o;
       buf.append(id.toString());
     }
    map_context.put("IMPORTS",buf.toString());
-   
+
    if (local_tests != null) {
       CompilationUnit tcu = (CompilationUnit) local_tests.getStructure();
       StringBuffer tbuf = new StringBuffer();
       for (Object o : tcu.imports()) {
-         ImportDeclaration id = (ImportDeclaration) o;
-         tbuf.append(id.toString());
+	 ImportDeclaration id = (ImportDeclaration) o;
+	 tbuf.append(id.toString());
        }
       map_context.put("LOCALTESTIMPORTS",tbuf.toString());
     }
@@ -276,12 +277,12 @@ private void setup(CompilationUnit cu)
       CompilationUnit tcu = (CompilationUnit) global_tests.getStructure();
       StringBuffer tbuf = new StringBuffer();
       for (Object o : tcu.imports()) {
-         ImportDeclaration id = (ImportDeclaration) o;
-         tbuf.append(id.toString());
+	 ImportDeclaration id = (ImportDeclaration) o;
+	 tbuf.append(id.toString());
        }
       map_context.put("GLOBALTESTIMPORTS",tbuf.toString());
     }
-   
+
    generateReadme();
 }
 
@@ -298,34 +299,34 @@ private void generateReadme()
       SumpData pmd = pattern_model.getModelData();
       pw.println("Name: " + smd.getName());
       if (pmd.getSource() != null) {
-         pw.println("Model File: " + pmd.getSource());
+	 pw.println("Model File: " + pmd.getSource());
        }
       for (String src : smd.getSources()) {
-         pw.println("Cose Source: " + src);
+	 pw.println("Cose Source: " + src);
        }
       for (LidsLibrary lib : smd.getLibraries()) {
-         pw.println("Library: " + lib.getFullId());
+	 pw.println("Library: " + lib.getFullId());
        }
       for (String s : smd.getMissingImports()) {
-         pw.println("Missing: " + s);
+	 pw.println("Missing: " + s);
        }
       CoseRequest req = smd.getCoseRequest();
       pw.println("Search Type: " + req.getCoseSearchType());
       pw.println("Scope Type: " + req.getCoseScopeType());
       pw.println("Language: " + req.getLanguage());
       for (CoseKeywordSet cks : req.getCoseKeywordSets()) {
-         pw.print("Keywords:");
-         for (String s : cks.getWords()) {
-            if (s.contains(" ")) {
-               pw.print(" \"" + s + "\"");
-             }
-            else pw.print(" " + s);
-          }
-         pw.println();
+	 pw.print("Keywords:");
+	 for (String s : cks.getWords()) {
+	    if (s.contains(" ")) {
+	       pw.print(" \"" + s + "\"");
+	     }
+	    else pw.print(" " + s);
+	  }
+	 pw.println();
        }
       pw.print("Key terms:");
       for (String s : req.getKeyTerms()) {
-         pw.print(" " + s);
+	 pw.print(" " + s);
        }
       pw.println();
       pw.println("Match score: " + map_context.get("SCORE"));
@@ -338,21 +339,21 @@ private void generateReadme()
 private void producePackageFiles(CompilationUnit cu)
 {
    File dir = (File) map_context.get("SRCDIR");
-   
+
    for (Object o : cu.types()) {
       AbstractTypeDeclaration td = (AbstractTypeDeclaration) o;
       String cnm = td.getName().getIdentifier();
       File f = new File(dir,cnm + ".java");
       try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
-        pw.println(map_context.get("PACKAGESTMT"));
-        pw.println(map_context.get("IMPORTS"));
-        pw.println();
-        pw.write(td.toString());
-        pw.println();
-        pw.println("// end of " + cnm + ".java");
+	pw.println(map_context.get("PACKAGESTMT"));
+	pw.println(map_context.get("IMPORTS"));
+	pw.println();
+	pw.write(td.toString());
+	pw.println();
+	pw.println("// end of " + cnm + ".java");
        }
       catch (IOException e) {
-         IvyLog.logE("Problem writing " + cnm,e);
+	 IvyLog.logE("Problem writing " + cnm,e);
        }
     }
 }
@@ -365,55 +366,55 @@ private void produceTestFiles()
    if (tests != null && tests.size() > 0) {
       File dir = (File) map_context.get("TESTDIR");
       for (File f : tests) {
-         File f1 = new File(dir,f.getName());
-         try {
-            IvyFile.copyFile(f,f1);
-          }
-         catch (IOException e) {
-            IvyLog.logE("Problem copying test file " + f,e);
-          }
+	 File f1 = new File(dir,f.getName());
+	 try {
+	    IvyFile.copyFile(f,f1);
+	  }
+	 catch (IOException e) {
+	    IvyLog.logE("Problem copying test file " + f,e);
+	  }
        }
     }
    if (local_tests != null) {
-      CompilationUnit cu = (CompilationUnit) local_tests.getStructure(); 
+      CompilationUnit cu = (CompilationUnit) local_tests.getStructure();
       File dir = (File) map_context.get("LOCALTESTDIR");
-      
+
       for (Object o : cu.types()) {
-         AbstractTypeDeclaration td = (AbstractTypeDeclaration) o;
-         String cnm = td.getName().getIdentifier();
-         File f = new File(dir,cnm + ".java");
-         try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
-            pw.println(map_context.get("PACKAGESTMT"));
-            pw.println(map_context.get("LOCALTESTIMPORTS"));
-            pw.println();
-            pw.write(td.toString());
-            pw.println();
-            pw.println("// end of " + cnm + ".java");
-          }
-         catch (IOException e) {
-            IvyLog.logE("Problem writing " + cnm,e);
-          }
+	 AbstractTypeDeclaration td = (AbstractTypeDeclaration) o;
+	 String cnm = td.getName().getIdentifier();
+	 File f = new File(dir,cnm + ".java");
+	 try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
+	    pw.println(map_context.get("PACKAGESTMT"));
+	    pw.println(map_context.get("LOCALTESTIMPORTS"));
+	    pw.println();
+	    pw.write(td.toString());
+	    pw.println();
+	    pw.println("// end of " + cnm + ".java");
+	  }
+	 catch (IOException e) {
+	    IvyLog.logE("Problem writing " + cnm,e);
+	  }
        }
     }
    if (global_tests != null && local_tests != null) {
-      CompilationUnit cu = (CompilationUnit) local_tests.getStructure(); 
+      CompilationUnit cu = (CompilationUnit) local_tests.getStructure();
       File dir = (File) map_context.get("GLOBALTESTDIR");
-      
+
       for (Object o : cu.types()) {
-         AbstractTypeDeclaration td = (AbstractTypeDeclaration) o;
-         String cnm = td.getName().getIdentifier();
-         File f = new File(dir,cnm + ".java");
-         try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
-            pw.println(map_context.get("PACKAGESTMT"));
-            pw.println(map_context.get("GLOBALTESTIMPORTS"));
-            pw.println();
-            pw.write(td.toString());
-            pw.println();
-            pw.println("// end of " + cnm + ".java");
-          }
-         catch (IOException e) {
-            IvyLog.logE("Problem writing " + cnm,e);
-          }
+	 AbstractTypeDeclaration td = (AbstractTypeDeclaration) o;
+	 String cnm = td.getName().getIdentifier();
+	 File f = new File(dir,cnm + ".java");
+	 try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
+	    pw.println(map_context.get("PACKAGESTMT"));
+	    pw.println(map_context.get("GLOBALTESTIMPORTS"));
+	    pw.println();
+	    pw.write(td.toString());
+	    pw.println();
+	    pw.println("// end of " + cnm + ".java");
+	  }
+	 catch (IOException e) {
+	    IvyLog.logE("Problem writing " + cnm,e);
+	  }
        }
     }
 }
@@ -429,9 +430,9 @@ private void producePomFile()
       if (ins != null) fr = new InputStreamReader(ins);
       else fr = new FileReader(IvyFile.expandName(TEMPLATE_FILE));
       FileWriter fw = new FileWriter(pom);
-      
+
       Velocity.evaluate(map_context,fw,"stare.pom",fr);
-      
+
       fr.close();
       fw.close();
     }
@@ -444,9 +445,9 @@ private void producePomFile()
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      File methods                                                            */
-/*                                                                              */
+/*										*/
+/*	File methods								*/
+/*										*/
 /********************************************************************************/
 
 private File getDirectory()
@@ -457,23 +458,23 @@ private File getDirectory()
       File dir = new File(nm);
       dir.mkdirs();
       for ( ; ; ) {
-         int n = random_gen.nextInt(1000000);
-         String s = TEST_DIRECTORY.replace("#",Integer.toString(n));
-         File f = new File(dir,s);
-         if (!f.exists() && f.mkdir()) {
-            work_directory = f;
-            break;
-          }
+	 int n = random_gen.nextInt(1000000);
+	 String s = TEST_DIRECTORY.replace("#",Integer.toString(n));
+	 File f = new File(dir,s);
+	 if (!f.exists() && f.mkdir()) {
+	    work_directory = f;
+	    break;
+	  }
        }
     }
-   return work_directory; 
+   return work_directory;
 }
 
 
 
 
 
-}       // end of class StareSolutionBase
+}	// end of class StareSolutionBase
 
 
 
