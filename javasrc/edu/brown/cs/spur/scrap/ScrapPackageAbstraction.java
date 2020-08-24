08fc0ca8d08ec39320e99f4db7fd90b7ead016a0
Steven Reiss <spr@cs.brown.edu>
2020-08-24 08:38:55 -0400

    Formatting.

--- javasrc/edu/brown/cs/spur/scrap/ScrapPackageAbstraction.java
+++ javasrc/edu/brown/cs/spur/scrap/ScrapPackageAbstraction.java
@@ -107,7 +107,7 @@ ScrapPackageAbstraction(ScrapAbstractor abs,CoseResult cr,CompilationUnit cu)
    uml_models = null;
    use_libraries = null;
    missing_imports = null;
-  
+
    initialize(cr,cu);
 }
 
@@ -139,32 +139,32 @@ ScrapPackageAbstraction(ScrapAbstractor abs,CoseResult cr,CompilationUnit cu)
    if (spa == this) return true;
 
    ScrapMergeData md = new ScrapMergeData();
-   
+
    RowelMatcher<PackageClass> rm = new RowelMatcher<>(use_classes,spa.use_classes);
    Map<PackageClass,PackageClass> map = rm.bestMatch(MatchType.MATCH_EXACT);
    for (Map.Entry<PackageClass,PackageClass> ent : map.entrySet()) {
       md.addMapping(ent.getKey(),ent.getValue());
     }
-    
+
    rm = new RowelMatcher<>(md.removeUsed(all_classes),md.removeUsed(spa.all_classes));
    map = rm.bestMatch(MatchType.MATCH_EXACT);
    for (Map.Entry<PackageClass,PackageClass> ent : map.entrySet()) {
       md.addMapping(ent.getKey(),ent.getValue());
-    }  
-   
+    }
+
    rm = new RowelMatcher<>(md.removeUsed(use_classes),md.removeUsed(spa.use_classes));
    map = rm.bestMatch(MatchType.MATCH_APPROXIMATE);
    for (Map.Entry<PackageClass,PackageClass> ent : map.entrySet()) {
       md.addMapping(ent.getKey(),ent.getValue());
     }
-   // 
+   //
    // matchItems(use_classes,spa.use_classes,md,true,false);
    // matchItems(use_classes,spa.use_classes,md,false,false);
    // matchItems(all_classes,spa.all_classes,md,false,false);
    // matchItems(use_classes,spa.use_classes,md,false,true);
-   // 
+   //
    if (!checkForMatch(md,spa)) return false;
-   
+
    String p1 = getCoseResult().getSource().getProjectId();
    String p2 = spa.getCoseResult().getSource().getProjectId();
    if (p1.equals(p2)) {
@@ -173,12 +173,12 @@ ScrapPackageAbstraction(ScrapAbstractor abs,CoseResult cr,CompilationUnit cu)
       SwiftScorer scorer = new SwiftScorer(t1,(ASTNode) getCoseResult().getStructure(),false);
       double v = scorer.getScore(t2,(ASTNode) spa.getCoseResult().getStructure());
       if (v > 0.99) {
-         return true;
+	 return true;
        }
     }
-   
+
    if (!actualMergeWith(md,spa)) return false;
-   
+
    return true;
 }
 
@@ -189,7 +189,7 @@ private boolean checkForMatch(ScrapMergeData md,ScrapPackageAbstraction spa)
    SumpParameters sp = scrap_abstractor.getParameters();
    double f1 = sp.getClassMinMatch();
    double f2 = sp.getClassMaxMatch();
-   
+
    if (!checkMatch(md,use_classes,spa.use_classes,f1,f2,0.0)) return false;
    return true;
 }
@@ -202,12 +202,12 @@ private boolean actualMergeWith(ScrapMergeData md,ScrapPackageAbstraction spa)
       PackageClass npc = (PackageClass) md.getMapping(pc);
       if (npc != null) npc.mergeWith(pc);
       else if (md.isAdded(pc)) {
-         use_classes.add(pc);
+	 use_classes.add(pc);
        }
     }
-   
+
    superMergeWith(spa);
-   
+
    return true;
 }
 
@@ -304,7 +304,7 @@ private void findComponents()
       switch (pc.getAbstraction().getArgType()) {
 	 case USERTYPE :
 	 case THISTYPE :
-            if (isClassIgnorable(pc)) continue;
+	    if (isClassIgnorable(pc)) continue;
 	    break;
 	 case EXCEPTION :
 	 case NUMBER :
@@ -313,10 +313,10 @@ private void findComponents()
 	    // remove exceptions, enums (if there), instances of standard classes
 	    continue;
        }
-      
+
       use.add(pc);
     }
-   
+
    for (int i = 0; i < use.size(); ++i) {
       PackageClass pc1 = use.get(i);
       for (int j = i+1; j < use.size(); ++j) {
@@ -330,9 +330,9 @@ private void findComponents()
        }
     }
    use.removeAll(remove);
-   
+
    addUsedTypes(use);
-   
+
    if (use.isEmpty())
       use = all_classes;
 
@@ -354,69 +354,69 @@ private boolean isClassIgnorable(PackageClass pc)
     }
    else if (atd instanceof EnumDeclaration) {
       return true;
-    }  
+    }
    else return true;
-   
-   int methods = 0; 
+
+   int methods = 0;
    int smethods = 0;
    int cnsts = 0;
    for (Object o : atd.bodyDeclarations()) {
       BodyDeclaration bd = (BodyDeclaration) o;
       if (bd instanceof MethodDeclaration) {
-         MethodDeclaration md = (MethodDeclaration) bd;
-         if (Modifier.isStatic(bd.getModifiers())) ++smethods;
-         else if (md.isConstructor()) ++cnsts;
-         else ++methods;
+	 MethodDeclaration md = (MethodDeclaration) bd;
+	 if (Modifier.isStatic(bd.getModifiers())) ++smethods;
+	 else if (md.isConstructor()) ++cnsts;
+	 else ++methods;
        }
     }
-   
+
    if (methods == 0 && (iface || abst)) return true;
    if (methods == 0 && smethods == 0) return true;
    if (methods == 0 && cnsts == 0) return true;
    if (methods == 0) return true;
-   
+
    CoseScores cs = pc.getScores();
    if (cs.getBoolean("TESTCASE")) return true;
-      
+
    return false;
 }
 
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Add enumeration types that are used by used classes                     */
-/*                                                                              */
+/*										*/
+/*	Add enumeration types that are used by used classes			*/
+/*										*/
 /********************************************************************************/
 
 private Set<PackageClass> addUsedTypes(List<PackageClass> use)
 {
    FindUsedTypes finder = new FindUsedTypes();
-   
+
    for (PackageClass pc : use) {
       finder.setClass(pc.getAbstraction());
       pc.getAstNode().accept(finder);
     }
-   
+
    Set<PackageClass> req = new HashSet<>();
-   
+
    for ( ; ; ) {
       List<PackageClass> added = new ArrayList<>();
       for (PackageClass pc : all_classes) {
-         JcompType jt = JcompAst.getJavaType(pc.getAstNode());
-         if (finder.isUsed(jt)) {
-            if (use.contains(pc)) req.add(pc);
-            else added.add(pc);
-          }
+	 JcompType jt = JcompAst.getJavaType(pc.getAstNode());
+	 if (finder.isUsed(jt)) {
+	    if (use.contains(pc)) req.add(pc);
+	    else added.add(pc);
+	  }
        }
       if (added.isEmpty()) break;
       for (PackageClass pc : added) {
-         use.add(pc);
-         finder.setClass(pc.getAbstraction());
-         pc.getAstNode().accept(finder);
+	 use.add(pc);
+	 finder.setClass(pc.getAbstraction());
+	 pc.getAstNode().accept(finder);
        }
     }
-   
+
    return req;
 }
 
@@ -426,55 +426,55 @@ private static class FindUsedTypes extends ASTVisitor {
 
    private Set<JcompType> used_types;
    private ScrapClassAbstraction for_class;
-   
+
    FindUsedTypes() {
       used_types = new HashSet<>();
       for_class = null;
     }
-   
-   boolean isUsed(JcompType jt)                 { return used_types.contains(jt); }
-   
-   void setClass(ScrapClassAbstraction sc)      { for_class = sc; }
-   
+
+   boolean isUsed(JcompType jt) 		{ return used_types.contains(jt); }
+
+   void setClass(ScrapClassAbstraction sc)	{ for_class = sc; }
+
    @Override public boolean visit(MethodDeclaration md) {
       if (for_class.isMethodUsed(md)) {
-         md.getReturnType2().accept(this);
-         for (Object o : md.parameters()) {
-            ASTNode n = (ASTNode) o;
-            n.accept(this);
-          }
+	 md.getReturnType2().accept(this);
+	 for (Object o : md.parameters()) {
+	    ASTNode n = (ASTNode) o;
+	    n.accept(this);
+	  }
        }
-         
+	
       return false;
     }
-   
+
    @Override public boolean visit(FieldDeclaration fd) {
       for (Object o : fd.fragments()) {
-         VariableDeclarationFragment vdf = (VariableDeclarationFragment) o;
-         if (for_class.isFieldUsed(vdf)) return true;
+	 VariableDeclarationFragment vdf = (VariableDeclarationFragment) o;
+	 if (for_class.isFieldUsed(vdf)) return true;
        }
       return false;
     }
-   
+
    @Override public void postVisit(ASTNode n) {
       JcompType jt = JcompAst.getJavaType(n);
-      if (jt != null && !jt.isPrimitiveType() && !jt.isBinaryType()) 
-         used_types.add(jt);
+      if (jt != null && !jt.isPrimitiveType() && !jt.isBinaryType())
+	 used_types.add(jt);
     }
-   
-}       // end of inner class FindEnums
+
+}	// end of inner class FindEnums
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      UML methods                                                             */
-/*                                                                              */
+/*										*/
+/*	UML methods								*/
+/*										*/
 /********************************************************************************/
 
-@Override List<SumpModel> getUmlModels() 
+@Override List<SumpModel> getUmlModels()
 {
    if (uml_models == null) buildUmlModels();
-   
+
    return uml_models;
 }
 
@@ -483,21 +483,21 @@ private static class FindUsedTypes extends ASTVisitor {
 {
    uml_models = new ArrayList<>();
    Map<String,SumpClass> cmap = new HashMap<>();
-   
+
    for (CoseResult cr : getAllResults()) {
       CompilationUnit cu = (CompilationUnit) cr.getStructure();
       SumpData sd = new SumpData(scrap_abstractor.getRequest(),cr,scrap_abstractor.getParameters());
       for (LidsLibrary ll : getReferencedLibraries()) {
-         sd.addLibrary(ll);
+	 sd.addLibrary(ll);
        }
       for (String s : missing_imports) sd.addMissingImport(s);
       SumpModel mdl = SumpFactory.createModel(sd);
       SumpPackage spe = mdl.setPackage("Default");
       for (PackageClass pc : use_classes) {
-         pc.addToUmlPackage(spe,cu,cmap);
+	 pc.addToUmlPackage(spe,cu,cmap);
        }
       for (PackageClass pc : use_classes) {
-         pc.addDependencies(spe,cu,cmap);
+	 pc.addDependencies(spe,cu,cmap);
        }
       mdl.setupImports(cu);
       mdl.computeLayout();
@@ -511,9 +511,9 @@ private static class FindUsedTypes extends ASTVisitor {
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Library methods                                                         */
-/*                                                                              */
+/*										*/
+/*	Library methods 							*/
+/*										*/
 /********************************************************************************/
 
 List<LidsLibrary> getReferencedLibraries()
@@ -522,23 +522,23 @@ List<LidsLibrary> getReferencedLibraries()
       use_libraries = new ArrayList<>();
       Set<String> imports = new HashSet<>();
       CoseResult usecr = null;
-      
+
       for (CoseResult cr : getAllResults()) {
-         CompilationUnit cu = (CompilationUnit) cr.getStructure();
-         PackageDeclaration pd = cu.getPackage();
-         String pnm = null;
-         if (pd != null) pnm = pd.getName().getFullyQualifiedName();
-         for (Object o : cu.imports()) {
-            ImportDeclaration id = (ImportDeclaration) o;
-            String idnm = id.getName().getFullyQualifiedName();
-            if (CoseConstants.isRelatedPackage(pnm,idnm)) continue;
-            if (CoseConstants.isStandardJavaLibrary(idnm)) continue;
-            if (id.isOnDemand()) idnm += ".*";
-            if (usecr == null) usecr = cr;
-            imports.add(idnm);
-          }
+	 CompilationUnit cu = (CompilationUnit) cr.getStructure();
+	 PackageDeclaration pd = cu.getPackage();
+	 String pnm = null;
+	 if (pd != null) pnm = pd.getName().getFullyQualifiedName();
+	 for (Object o : cu.imports()) {
+	    ImportDeclaration id = (ImportDeclaration) o;
+	    String idnm = id.getName().getFullyQualifiedName();
+	    if (CoseConstants.isRelatedPackage(pnm,idnm)) continue;
+	    if (CoseConstants.isStandardJavaLibrary(idnm)) continue;
+	    if (id.isOnDemand()) idnm += ".*";
+	    if (usecr == null) usecr = cr;
+	    imports.add(idnm);
+	  }
        }
-      
+
       LidsFinder fndr = new LidsFinder(usecr);
       for (String s : imports) fndr.addImportPath(s);
       use_libraries = fndr.findLibraries();
@@ -565,13 +565,13 @@ List<LidsLibrary> getReferencedLibraries()
    for (PackageClass pc : use_classes) {
       pc.outputClass();
     }
-   
+
    System.err.println("--------OTHER CLASSES");
    for (PackageClass pc : all_classes) {
       if (use_classes.contains(pc)) continue;
       pc.outputClass();
     }
-   
+
    System.err.println("---------LIBRARIES");
    for (LidsLibrary lib : use_libraries) {
       System.err.println("\t" + lib.getId());
@@ -580,7 +580,7 @@ List<LidsLibrary> getReferencedLibraries()
    for (String imp : missing_imports) {
       System.err.println("\t" + imp);
     }
-   
+
    System.err.println("--------MODELS");
    for (SumpModel mdl : uml_models) {
       IvyXmlWriter xw = new IvyXmlWriter();
@@ -596,9 +596,9 @@ List<LidsLibrary> getReferencedLibraries()
       StringWriter xmiw = new StringWriter();
       mdl.generateXMI(xmiw);
       System.err.println(xmiw.toString());
-      break;            // only output one model for now
+      break;		// only output one model for now
     }
-   
+
    System.err.println();
 }
 
@@ -624,36 +624,36 @@ private static class PackageClass extends ScrapComponent {
       addName(atd.getName().getIdentifier());
     }
 
-   @Override ComponentType getComponentType()	{ return ComponentType.CLASS; } 
+   @Override ComponentType getComponentType()	{ return ComponentType.CLASS; }
 
    ScrapClassAbstraction getAbstraction()	{ return class_abstraction; }
-   
-   AbstractTypeDeclaration getAstNode()         { return type_decls.get(0); }
+
+   AbstractTypeDeclaration getAstNode() 	{ return type_decls.get(0); }
 
    @Override boolean isCompatibleWith(ScrapComponent sc) {
       if (sc instanceof PackageClass) {
-         PackageClass pc = (PackageClass) sc;
-         if (class_abstraction != pc.class_abstraction) return false;
-         return true;
+	 PackageClass pc = (PackageClass) sc;
+	 if (class_abstraction != pc.class_abstraction) return false;
+	 return true;
        }
       return false;
     }
-   
+
    @Override void mergeWith(ScrapComponent sc) {
       PackageClass pc = (PackageClass) sc;
       type_decls.addAll(pc.type_decls);
       super.mergeWith(sc);
     }
-   
+
    boolean isSubtypeOf(PackageClass pc) {
       for (AbstractTypeDeclaration atd : type_decls) {
-         for (AbstractTypeDeclaration atd1 : pc.type_decls) {
-            if (atd.getAST() == atd1.getAST()) {
-               JcompType jt = JcompAst.getJavaType(atd);
-               JcompType jt1 = JcompAst.getJavaType(atd1);
-               if (jt.isCompatibleWith(jt1)) return true;
-             }
-          }
+	 for (AbstractTypeDeclaration atd1 : pc.type_decls) {
+	    if (atd.getAST() == atd1.getAST()) {
+	       JcompType jt = JcompAst.getJavaType(atd);
+	       JcompType jt1 = JcompAst.getJavaType(atd1);
+	       if (jt.isCompatibleWith(jt1)) return true;
+	     }
+	  }
        }
       return false;
     }
@@ -661,34 +661,34 @@ private static class PackageClass extends ScrapComponent {
    void outputClass() {
       System.err.println("CLASS----" + getNames());
       if (class_abstraction == null)
-         System.err.println("\tNO ABSTRACTION");
+	 System.err.println("\tNO ABSTRACTION");
       else
-         class_abstraction.outputShortAbstraction();
+	 class_abstraction.outputShortAbstraction();
     }
-   
-   void addToUmlPackage(SumpPackage pkg,CompilationUnit cu,Map<String,SumpClass> cmap) { 
+
+   void addToUmlPackage(SumpPackage pkg,CompilationUnit cu,Map<String,SumpClass> cmap) {
       for (AbstractTypeDeclaration atd : type_decls) {
-         if (atd.getAST() == cu.getAST()) {
-            SumpClass sc = pkg.addClass(atd);
-            if (class_abstraction != null) {
-               class_abstraction.addToUmlClass(sc,atd);
-             }
-            String nm = atd.getName().getIdentifier();
-            cmap.put(nm,sc);
-          }
+	 if (atd.getAST() == cu.getAST()) {
+	    SumpClass sc = pkg.addClass(atd);
+	    if (class_abstraction != null) {
+	       class_abstraction.addToUmlClass(sc,atd);
+	     }
+	    String nm = atd.getName().getIdentifier();
+	    cmap.put(nm,sc);
+	  }
        }
     }
-   
-   
+
+
    void addDependencies(SumpPackage pkg,CompilationUnit cu,Map<String,SumpClass> cmap)
    {
       for (AbstractTypeDeclaration atd : type_decls) {
-         if (atd.getAST() == cu.getAST()) {
-            pkg.addDependencies(atd,cmap);
-          }
+	 if (atd.getAST() == cu.getAST()) {
+	    pkg.addDependencies(atd,cmap);
+	  }
        }
    }
-   
+
 }	// end of inner class PackageClass;
 
 
@@ -702,344 +702,3 @@ private static class PackageClass extends ScrapComponent {
 
 
 /* end of ScrapPackageAbstraction.java */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
